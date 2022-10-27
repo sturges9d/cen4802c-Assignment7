3b9912f63da3a99e8de65964cfb18e6a65eb4288
Stephen Sturges Jr <stephen.sturges.jr@outlook.com>
2022-10-27 11:30:07 -0400

    Initial commit.

---
+++ src/main/java/cen4802c/Assignment7/App.java
@@ -0,0 +1,13 @@
+package cen4802c.Assignment7;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
